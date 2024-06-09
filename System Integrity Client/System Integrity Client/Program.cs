ceb20880956900971dd671d812347c7cbcbaad53
aSpired92 <maciek120920@gmail.com>
2024-06-09 23:53:31 +0200

    Add files via upload

---
+++ System Integrity Client/System Integrity Client/Program.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using static System.Net.WebRequestMethods;
+
+namespace System_Integrity_Client
+{
+    internal class Program
+    {
+        private const string ip = "127.0.0.1";
+        private const int port = 5000;
+
+
+        // checksums lock
+        private static object _myLock = new object();
+        private static Dictionary<string, string> checksums = new Dictionary<string, string>();
+
+        private static string systemPath = Environment.GetFolderPath(Environment.SpecialFolder.Windows) + @"\System32";
+
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Zbieranie informacji o plikach systemowych...");
+            GetChecksums(systemPath);
+            
+            Console.WriteLine("Wysyłanie danych na serwer...");
+            string data = SerializeChecksums();
+            await SendData(Encoding.UTF8.GetBytes(data));
+
+            Console.WriteLine("Wysyłano dane. Naciśnij dowolny klawisz aby kontynuować.");
+            Console.ReadKey();
+        }
+
+        private static RSAParameters RSAParametersFromString(string str)
+        {
+            var sr = new System.IO.StringReader(str);
+            var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+            return (RSAParameters)xs.Deserialize(sr);
+        }
+
+        public static async Task SendData(byte[] data)
+        {
+            TcpClient client = new TcpClient();
+            await client.ConnectAsync(ip, port);
+
+            using (RSA rsa = RSA.Create(2048)) 
+            {
+                using (Aes aes = Aes.Create()) 
+                {
+                    aes.Padding = PaddingMode.PKCS7;
+                    using (NetworkStream stream = client.GetStream())
+                    {
+                        List<byte> serverRSAKey = new List<byte>();
+                        byte[] buffer = new byte[1024];
+                        int bytesRead;
+                        while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                        {
+                            for (int i = 0; i < bytesRead; i++)
+                            {
+                                serverRSAKey.Add(buffer[i]);
+                            }
+
+                            if(bytesRead < buffer.Length)
+                            {
+                                break;
+                            }
+                        }
+
+                        RSAParameters param = RSAParametersFromString(Encoding.UTF8.GetString(serverRSAKey.ToArray()));
+                        rsa.ImportParameters(param);
+
+                        byte[] encrytpedAesKey = rsa.Encrypt(aes.Key, RSAEncryptionPadding.OaepSHA512);
+                        byte[] encrytpedAesIV = rsa.Encrypt(aes.IV, RSAEncryptionPadding.OaepSHA512);
+
+                        await stream.WriteAsync(encrytpedAesKey, 0, encrytpedAesKey.Length);
+                        await stream.ReadAsync(buffer, 0, buffer.Length);
+                        await stream.WriteAsync(encrytpedAesIV, 0, encrytpedAesIV.Length);
+                        await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                        ICryptoTransform encryptor = aes.CreateEncryptor();
+                        byte[] encryptedData = encryptor.TransformFinalBlock(data, 0, data.Length);
+                        await stream.WriteAsync(encryptedData, 0, encryptedData.Length);
+                    }
+                }
+            }
+            
+            client.Close();
+        }
+
+        private static string SerializeChecksums()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (KeyValuePair<string, string> entry in checksums)
+            {
+                stringBuilder.Append(entry.Key + "|" + entry.Value + "\t");
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private static void GetChecksums(string path)
+        {
+            List<string> directories = GetDirectories(path);
+            Task[] taskArray = new Task[directories.Count];
+
+            for (int i = 0; i < directories.Count; i++)
+            {
+                string d = directories[i];
+
+                taskArray[i] = Task.Factory.StartNew((Object directory) =>
+                {
+                    try
+                    {
+                        string strDir = directory as string;
+
+                        string[] files = Directory.GetFiles(strDir);
+
+                        foreach (string p in files)
+                        {
+                            string temp = GetChecksum(p);
+
+                            if (temp.Equals(string.Empty))
+                            {
+                                continue;
+                            }
+
+                            lock (_myLock)
+                            {
+                                if (checksums.ContainsKey(p))
+                                {
+                                    continue;
+                                }
+
+                                checksums.Add(p, temp);
+                            }
+                        }
+                    }
+                    catch (UnauthorizedAccessException ex)
+                    {
+                        //Console.WriteLine("Couldn't open " + path + ". Access Denied.");
+                    }
+                }, d);
+            }
+
+            Task.WaitAll(taskArray);
+        }
+
+        private static List<string> GetDirectories(string path)
+        {
+            List<string> directories = new List<string>();
+
+            try
+            {
+                foreach (string p in Directory.GetDirectories(path))
+                {
+                    directories.Add(p);
+                    foreach (string d in Directory.GetDirectories(p))
+                    {
+                        directories.Add(d);
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                //Console.WriteLine("Couldn't enter " + path + ". Access Denied.");
+            }
+
+            return directories;
+        }
+
+        private static string GetChecksum(string path)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                try
+                {
+                    using (FileStream fs = new FileStream(path, FileMode.Open))
+                    {
+                        byte[] buff = new byte[fs.Length];
+                        fs.Read(buff, 0, (int)fs.Length);
+
+                        byte[] checksum = md5.ComputeHash(buff);
+
+                        return BitConverter.ToString(checksum).Replace("-", "").ToLower();
+                    }
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    //Console.WriteLine("Couldn't open " + path + ". Access Denied.");
+                    return string.Empty;
+                }
+                catch (IOException ex)
+                {
+                    //Console.WriteLine("Couldn't open " + path + ". It is being used by another process.");
+                    return string.Empty;
+                }
+            }
+        }
+    }
+}
---
+++ System Integrity Server/System Integrity Server/Program.cs
@@ -0,0 +1,250 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using System;
+
+namespace System_Integrity_Server
+{
+    internal class Program
+    {
+        private const int Port = 5000;
+
+        private static object _lockFile = new object();
+        private static string checksumFile = @"./checksums.txt";
+        private static Dictionary<string, string> checksums = new Dictionary<string, string>();
+
+        static async Task Main(string[] args)
+        {
+            if (File.Exists(checksumFile)) 
+            {
+                using (FileStream fs = new FileStream(checksumFile, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs)) 
+                    {
+                        string temp = sr.ReadToEnd();
+                        checksums = DeserializeChecksums(temp);
+                    }
+                }
+            }
+            await StartServerAsync();
+        }
+
+        private static async Task StartServerAsync()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, Port);
+            listener.Start();
+            Console.WriteLine("Serwer uruchomiony, oczekiwanie na połączenia...");
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                Console.WriteLine("Połączono z klientem.");
+                _ = HandleClientAsync(client);
+            }
+        }
+
+        private static string RSAParametersToString(RSAParameters param)
+        {
+            var sw = new System.IO.StringWriter();
+            var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+            xs.Serialize(sw, param);
+            return sw.ToString();
+        }
+
+        private static async Task HandleClientAsync(TcpClient client)
+        {
+            IPEndPoint endPoint = (IPEndPoint)client.Client.RemoteEndPoint;
+
+            string clientIP = endPoint.Address.ToString();
+            string message = @"";
+
+            using (RSA rsa = RSA.Create(2048)) 
+            {
+                RSAParameters serverPrivateKey = rsa.ExportParameters(true);
+                RSAParameters serverPublicKey = rsa.ExportParameters(false);
+
+                using (NetworkStream stream = client.GetStream())
+                {
+                    Console.WriteLine(clientIP + ": Wysyłanie klucza publicznego");
+                    byte[] serverPublicKeyBytes = Encoding.UTF8.GetBytes(RSAParametersToString(serverPublicKey));
+                    await stream.WriteAsync(serverPublicKeyBytes, 0, serverPublicKeyBytes.Length);
+
+                    Console.WriteLine(clientIP + ": Odczytywanie klucza szyfrującego AES");
+                    List<byte> aesKeyEncrypted = new List<byte>();
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                    {
+                        for(int i=0; i<bytesRead; i++) 
+                        {
+                            aesKeyEncrypted.Add(buffer[i]);
+                        }
+
+                        if (bytesRead < buffer.Length)
+                        {
+                            break;
+                        }
+                    }
+                    byte[] aesKey = rsa.Decrypt(aesKeyEncrypted.ToArray(), RSAEncryptionPadding.OaepSHA512);
+
+                    byte[] response = Encoding.UTF8.GetBytes("OK");
+                    await stream.WriteAsync(response, 0, response.Length);
+
+                    Console.WriteLine(clientIP + ": Odczytywanie IV AES");
+                    List<byte> aesIVEncrypted = new List<byte>();
+                    buffer = new byte[1024];
+                    while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                    {
+                        for (int i = 0; i < bytesRead; i++)
+                        {
+                            aesIVEncrypted.Add(buffer[i]);
+                        }
+
+                        if (bytesRead < buffer.Length)
+                        {
+                            break;
+                        }
+                    }
+                    byte[] aesIV = rsa.Decrypt(aesIVEncrypted.ToArray(), RSAEncryptionPadding.OaepSHA512);
+
+                    await stream.WriteAsync(response, 0, response.Length);
+
+                    Console.WriteLine(clientIP + ": Odbieranie danych");
+                    List<byte> dataEncrypted = new List<byte>();
+                    buffer = new byte[1024];
+                    while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                    {
+                        for (int i = 0; i < bytesRead; i++)
+                        {
+                            dataEncrypted.Add(buffer[i]);
+                        }
+
+                        if (bytesRead < buffer.Length)
+                        {
+                            break;
+                        }
+                    }
+
+                    byte[] data;
+                    using (Aes aes = Aes.Create())
+                    {
+                        aes.Padding = PaddingMode.PKCS7;
+                        aes.Key = aesKey;
+                        aes.IV = aesIV;
+
+                        ICryptoTransform decryptor = aes.CreateDecryptor();
+
+                        data = decryptor.TransformFinalBlock(dataEncrypted.ToArray(), 0, dataEncrypted.ToArray().Length);
+                    }
+
+                    message = Encoding.UTF8.GetString(data);
+                }
+            }
+            
+            
+
+            client.Close(); 
+            Console.WriteLine(clientIP + ": Klient rozłączony");
+            Console.WriteLine(clientIP + ": Sprawdzanie sum kontrolnych");
+            Dictionary<string, string> sentChecksums = DeserializeChecksums(message);
+
+            List<string> wrongChecksums = CompareChecksums(sentChecksums);
+
+            if (wrongChecksums.Count > 0)
+            {
+                if (!EventLog.SourceExists("System Integrity"))
+                {
+                    EventLog.CreateEventSource("System Integrity", "MyNewLog");
+                }
+
+                EventLog eventLog = new EventLog();
+                eventLog.Source = "System Integrity";
+                string msg = clientIP + ": Niektóre pliki systemowe mają inne sumy kontrolne:\n\n";
+
+                foreach (string checksum in wrongChecksums) 
+                {
+                    msg += checksum + "\n";
+                }
+
+                Console.WriteLine(msg);
+                eventLog.WriteEntry(msg, EventLogEntryType.Error);
+            }
+
+            lock(_lockFile)
+            {
+                using(FileStream fs = new FileStream(checksumFile,FileMode.Create))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs)) 
+                    {
+                        sw.Write(SerializeChecksums());
+                    }
+                }
+            }
+        }
+
+        private static Dictionary<string, string> DeserializeChecksums(string data)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            string[] lines = data.Split('\t');
+            foreach (string line in lines)
+            {
+                try
+                {
+                    string[] parts = line.Split('|');
+
+                    if (parts.Length < 2)
+                    {
+                        continue;
+                    }
+
+                    string path = parts[0];
+                    string checksum = parts[1];
+
+                    result.Add(path, checksum);
+                }
+                catch
+                {}
+            }
+
+            return result;
+        }
+
+        private static string SerializeChecksums()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (KeyValuePair<string, string> entry in checksums)
+            {
+                stringBuilder.Append(entry.Key + "|" + entry.Value + "\t");
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private static List<string> CompareChecksums(Dictionary<string, string> data) 
+        {
+            List<string> result = new List<string>();
+            
+            foreach (KeyValuePair<string, string> entry in data) 
+            {
+                if(!checksums.ContainsKey(entry.Key))
+                {
+                    checksums.Add(entry.Key, entry.Value);
+                    continue;
+                }
+
+                if (!entry.Value.Equals(checksums[entry.Key]))
+                {
+                    result.Add(entry.Key);
+                }
+            }
+
+            return result;
+        }
+    }
+}
